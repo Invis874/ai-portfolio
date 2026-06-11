cf9e400eb4879914df93139ecedb16114aef19d7
Invisible <akulenko_874@mail.ru>
2026-06-11 23:30:32 +0500

    setting

--- backend/django_project/settings_prod.py
+++ backend/django_project/settings_prod.py
@@ -90,7 +90,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://ai-portfolio-git-prod-akulenkomakar-1999s-projects.vercel.app",
     "https://ai-portfolio-omega-ten.vercel.app/",
-    "ai-portfolio-5lx0slx1d-akulenkomakar-1999s-projects.vercel.app"
+    "https://ai-portfolio-5lx0slx1d-akulenkomakar-1999s-projects.vercel.app"
 ]
 
 # REST Framework
